c9e82a7e9d903e235e08daa29427f21e5a84156b
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2021-05-03 16:19:38 +0530

    Add files via upload

--- content/res/variables/dh.js
+++ content/res/variables/dh.js
@@ -80,9 +80,80 @@ window["dh_cr" + Inc] = credit5;
 window["dh_l" + Inc] = "https://youtu.be/MYRghjCZjUE?t=80";
 Inc ++;
 
+window["dh_t" + Inc] = "Douglas Costa Skill";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/HQ-BVLMhOBE?t=30";
+Inc ++;
 
+window["dh_t" + Inc] = "Blizardo";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/NN4xJYh8Fmw?t=66";
+Inc ++;
 
+window["dh_t" + Inc] = "Extreme Rainbow";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/NN4xJYh8Fmw?t=127";
+Inc ++;
 
+window["dh_t" + Inc] = "The Hidden Chop";
+window["dh_cr" + Inc] = credit3;
+window["dh_l" + Inc] = "https://youtu.be/oQmrKNxXgCk?t=50";
+Inc ++;
 
-dh_total = 16; //Dribble easy total
+window["dh_t" + Inc] = "The Delayed Sleep";
+window["dh_cr" + Inc] = credit3;
+window["dh_l" + Inc] = "https://youtu.be/oQmrKNxXgCk?t=103";
+Inc ++;
+
+window["dh_t" + Inc] = "Clap Panna";
+window["dh_cr" + Inc] = credit3;
+window["dh_l" + Inc] = "https://youtu.be/oQmrKNxXgCk?t=188";
+Inc ++;
+
+window["dh_t" + Inc] = "The Push";
+window["dh_cr" + Inc] = credit3;
+window["dh_l" + Inc] = "https://youtu.be/oQmrKNxXgCk?t=252";
+Inc ++;
+
+window["dh_t" + Inc] = "Twist Power";
+window["dh_cr" + Inc] = credit3;
+window["dh_l" + Inc] = "https://youtu.be/oQmrKNxXgCk?t=304";
+Inc ++;
+
+window["dh_t" + Inc] = "The Flying Croquetta";
+window["dh_cr" + Inc] = credit3;
+window["dh_l" + Inc] = "https://youtu.be/IGB1I58GZ5A?t=108";
+Inc ++;
+
+window["dh_t" + Inc] = "Issy Hitman AKKA";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/IItYdcwdfVA?t=19";
+Inc ++;
+
+window["dh_t" + Inc] = "John Guidetti Rainbow";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/x47PQcoMIcw?t=29";
+Inc ++;
+
+window["dh_t" + Inc] = "AKKA 3000";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/x8gbSDHowTk?t=21";
+Inc ++;
+
+window["dh_t" + Inc] = "SideBreaker";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/ir264jqxfD0?t=42";
+Inc ++;
+
+window["dh_t" + Inc] = "Hazardous";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/U30laLvZ1bU?t=75";
+Inc ++;
+
+window["dh_t" + Inc] = "Helicopter Flick";
+window["dh_cr" + Inc] = credit5;
+window["dh_l" + Inc] = "https://youtu.be/z_E1eWYurC8?t=31";
+Inc ++;
+
+dh_total = 31; //Dribble hard total
 scriptLoaded = true;
